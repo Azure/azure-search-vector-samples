e89e0dd2f55d2f109ee4abe799514b235fcc7e24
Matt Gotteiner <matthew.gotteiner@microsoft.com>
2024-08-03 12:09:38 -0700

    update to support sas token

--- demo-python/code/indexers/document-intelligence-custom-skill/api/functions/function_app.py
+++ demo-python/code/indexers/document-intelligence-custom-skill/api/functions/function_app.py
@@ -2,24 +2,45 @@ import azure.functions as func
 import json
 import logging
 import os
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
 from azure.ai.documentintelligence.aio import DocumentIntelligenceClient
-from azure.identity import DefaultAzureCredential
+from azure.identity.aio import DefaultAzureCredential
 import base64
 import io
 from azure.core.exceptions import HttpResponseError
+from typing import Any
 
 app = func.FunctionApp()
 
 @app.function_name(name="read")
 @app.route(route="read")
 async def ReadDocument(req: func.HttpRequest) -> func.HttpResponse:
-    # Read input file
     input_files = {}
+    input_sas = {}
+    result_content = []
     try:
         req_body = req.get_json()
+        # Read input values
+        # Either file_data or metadata_storage_path and metadata_storage_sas_token
         if "values" in req_body:
             for value in req_body["values"]:
-                input_files[value["recordId"]] = value["data"]["file_data"]
+                record_id = value["recordId"]
+                if "file_data" in value["data"]:
+                    input_files[record_id] = value["data"]["file_data"]
+                elif "metadata_storage_path" in value["data"] and "metadata_storage_sas_token" in value["data"]:
+                    input_sas[record_id] = f"{value['data']['metadata_storage_path']}{value['data']['metadata_storage_sas_token']}"
+                else:
+                    result_content.append(
+                        {
+                            "recordId": record_id,
+                            "data": {},
+                            "errors": [
+                               {
+                                   "message": "Expected either 'file_data' or 'metadata_storage_path' and 'metadata_storage_sas_token'"
+                               } 
+                            ]
+                        }
+                    )
     except Exception as e:
         logging.exception("Could not get input data request body")
         return func.HttpResponse(
@@ -28,14 +49,15 @@ async def ReadDocument(req: func.HttpRequest) -> func.HttpResponse:
         )
 
     async with DocumentIntelligenceClient(endpoint=os.environ["AZURE_DOCUMENTINTELLIGENCE_ENDPOINT"], credential=DefaultAzureCredential()) as client:
-        result_content = []
         for record_id, file in input_files.items():
-            result_content.append(await process_file(client, record_id, file))
+            result_content.append(await process_file(client, record_id, file)) # type: ignore
+        for record_id, sas_uri in input_sas.items():
+            result_content.append(await process_sas_uri(client, record_id, sas_uri)) # type: ignore
 
     response = { "values": result_content }
     return func.HttpResponse(body=json.dumps(response), mimetype="application/json", status_code=200)
 
-async def process_file(client: DocumentIntelligenceClient, record_id: any, file: any):
+async def process_file(client: DocumentIntelligenceClient, record_id: str, file: Any):
     if not isinstance(file, dict) or \
         file["$type"] != "file" or \
         "data" not in file:
@@ -72,6 +94,28 @@ async def process_file(client: DocumentIntelligenceClient, record_id: any, file:
             "recordId": record_id, "data": { "content": result.content }
         }
     except HttpResponseError as e:
+        logging.exception("Failed to read document")
+        return {
+            "recordId": record_id,
+            "data": {},
+            "errors": [
+                {
+                    "message": f"Failed to process file content: {e.message}"
+                }
+            ]
+        }
+
+async def process_sas_uri(client: DocumentIntelligenceClient, record_id: str, sas_uri: str):
+    try:
+        poller = await client.begin_analyze_document(
+            "prebuilt-layout", analyze_request=AnalyzeDocumentRequest(url_source=sas_uri)
+        )
+        result = await poller.result()
+        return {
+            "recordId": record_id, "data": { "content": result.content }
+        }
+    except HttpResponseError as e:
+        logging.exception("Failed to read document")
         return {
             "recordId": record_id,
             "data": {},
